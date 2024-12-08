7b038f2f525b00e7dde4059c3bff8721ebd55e78
mkowieska <martyna.kowieska@gmail.com>
2024-12-08 16:30:31 +0100

    commit 6F

--- year3/AI-1/lab/lab6F/sql/01-post.sql
+++
@@ -1,8 +0,0 @@
-create table musicalbum
-(
-    id      integer not null
-        constraint musicalbum_pk
-            primary key autoincrement,
-    subject text not null,
-    content text not null
-);
--- year3/AI-1/lab/lab6F/templates/nav.html.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** @var $router \App\Service\Router */
-
-?>
-<ul>
-    <li><a href="<?= $router->generatePath('') ?>">Home</a></li>
-    <li><a href="<?= $router->generatePath('musicalbum-index') ?>">MusicAlbum</a></li>
-</ul>
-<?php
